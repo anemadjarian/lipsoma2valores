9eb2799b993ed77e65ec4cb0e7b2e3dac4291a18
Ane Madjarian Viana <madjarianviana@gmail.com>
2025-02-25 08:20:55 -0300

    soma

---
+++ soma.c
@@ -0,0 +1,10 @@
+
+int main (){
+    float valor1, valor2, soma;
+    printf("Digite o primeiro valor:\n");
+    scanf("%f", &valor1);
+    printf("Digite o segundo valor:\n");
+    scanf("%f", &valor2);
+    soma=valor1+valor2;
+    printf("A soma dos dois termos é: %f", soma);
+}
